c84b67260ec9ffa5473c3913f3455eccf1fa7f81
Leonardo Gomes Da Silva <35742346+LeonardoGomes99@users.noreply.github.com>
2020-03-28 21:18:47 -0300

    Att
    Att

---
+++ lossantoscustom/view/home.php
@@ -0,0 +1,15 @@
+<?php
+echo "<body style='background: url(src/public/background2.jpg);'>";
+?>
+
+<div class="homepageImg"><img src="src/public/h1.jpg" class="homeimg1" alt="image"></div>
+<div class="homepageImg"><img src="src/public/h2.jpg" class="homeimg2" alt="image"></div>
+<div class="homepageImg"><img src="src/public/h3.jpg" class="homeimg3" alt="image"></div>
+<div class="homepageImg"><img src="src/public/h4.jpg" class="homeimg4" alt="image"></div>
+
+<div class="boxcar">
+    <div class="boxcarimg"><img src="src/public/mcqueen.jpg" class="carimg" alt="image"></div>
+    <p class="modelo">Modelo: Relampâgo Crackinho</p>
+    <p class="marca">Marca: Crack</p>
+    <p class="preco">Preço: R$3000,00</p>
+</div>
