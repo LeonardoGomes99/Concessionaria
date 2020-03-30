efe150cba91906ee6a7ae0be8e54e3e826765f95
SotnDragon <jonatapqt01@gmail.com>
2020-03-30 10:38:04 -0300

    bugfixes

--- lossantoscustom/view/carro.php
+++ lossantoscustom/view/carro.php
@@ -13,15 +13,101 @@ $carro = $car->select_where("where id = '$id'")[0];
         background: url("src/public/background3.jpg") no-repeat center fixed;
         background-size: cover;
     }
+
+    .devcard {
+        margin: 30px;
+        margin-top: 20px;
+        position: relative;
+        display: inline-block;
+        width: 800px;
+        text-align: center;
+        background-color: rgba(0, 0, 0, 0.9);
+        box-shadow: 0px 0px 10px 2px #fff;
+        color: white;
+        border-radius: 3px;
+    }
+
+    .devcard img {
+        border-radius: 3px 3px 0px 0px;
+        width: 100%;
+
+        height: 280px;
+        object-fit: cover;
+        object-position: center;
+    }
+
+    .devcard .modelo {
+        margin-top: 25px;
+        margin-left: 25px;
+        margin-right: 25px;
+
+        text-align: left;
+        font-size: 30px;
+    }
+
+    .devcard .marca {
+        margin-top: 25px;
+        margin-left: 25px;
+        margin-right: 25px;
+
+        text-align: left;
+        font-size: 25px;
+    }
+
+    .devcard .preco {
+        margin-top: 25px;
+        margin-left: 25px;
+        margin-right: 25px;
+
+        text-align: left;
+        font-size: 20px;
+    }
+
+    .devcard .detalhes {
+        margin-top: 25px;
+        margin-left: 25px;
+        margin-right: 25px;
+        margin-bottom: 25px;
+       
+        font-size: 20px;
+        text-align: justify;
+        
+    }
+
+    .devs {
+        display: block;
+    }
+
+    .areas {
+        width: 100%;
+        display: block;
+        text-align: center;
+    }
 </style>
 
-<h1>carro</h1>
 
 
-<div class="carshow">
-    <div class="carshowinside"><img src="<?= $carro["imagem"] ?>" class="carimg" alt="image"></div>
-    <label class="showcarmodelo" for="cadcarmodelo">Modelo: <?= $carro["modelo"] ?></label>
-    <label class="showcarmarca" for="cadcarmarca">Marca Carro: <?= $carro["marca"] ?></label>
-    <label class="showcarpreco" for="cadcarpreco">Preço: <?= $carro["preco"] ?></label>
-    <label class="showcardetalhes" for="cadcardetalhes">Detalhes: <?= $carro["detalhes"] ?></label>
+<div class="areas">
+    <br>
+    <br>
+    <br>
+    <div class="devs">
+        <div class="devcard">
+            <div class="image">
+                <img src="<?= $carro["imagem"] ?>" class="carimg">
+            </div>
+            <div class="modelo">
+                Modelo: <?= $carro["modelo"] ?>
+            </div>
+            <div class="marca">
+                Marca: <?= $carro["marca"] ?>
+            </div>
+            <div class="preco">
+                A partir de: <?= $util->money_blr($carro["preco"]) ?>
+            </div>
+            <div class="detalhes">
+                Detalhes: <?= $carro["detalhes"] ?>
+            </div>
+        </div>
+    </div>
 </div>
